533dc2fe7d6989baa76bbddccc582c1510661017
carlos <carlitoosrg00@alumnos.ilerna.com>
2026-03-05 18:25:16 +0100

    Actualizo código

--- Videojuego/src/modelo/Personaje.java
+++ Videojuego/src/modelo/Personaje.java
@@ -2,4 +2,59 @@ package modelo;
 
 public class Personaje {
 
+	protected int vidaBueno;
+	protected int vidaMalo;
+	
+	public Personaje(int vidaBueno, int vidaMalo) {
+		this.vidaBueno = vidaBueno;
+		this.vidaMalo = vidaMalo;
+	}
+	
+	public int getVidaBueno() {
+		return vidaBueno;
+	}
+	
+	public void setVidaBueno(int vidaBueno) {
+		this.vidaBueno = vidaBueno;
+	}
+	
+	public int getVidaMalo() {
+		return vidaMalo;
+	}
+	
+	public void setVidaMalo(int vidaMalo) {
+		this.vidaMalo = vidaMalo;
+	}
+	
+	public void recibirDano(int dano) {
+		vidaBueno -= dano;
+		if (vidaBueno < 0) {
+			vidaBueno = 0;
+		}
+	}
+	
+	public void recibirDanoMalo(int dano) {
+		vidaMalo -= dano;
+		if (vidaMalo < 0) {
+			vidaMalo = 0;
+		}
+	}
+	
+	public boolean estaVivo() {
+		return vidaBueno > 0;
+	}
+	
+	public boolean estaVivoMalo() {
+		return vidaMalo > 0;
+	}
+	
+	public void atacar(Personaje enemigo, int dano) {
+		enemigo.recibirDano(dano);
+	}
+	
+	public void atacarMalo(Personaje enemigo, int dano) {
+		enemigo.recibirDanoMalo(dano);
+	}
+	
+
 }
--- Videojuego/src/vista/PanelEscena1.java
+++ Videojuego/src/vista/PanelEscena1.java
@@ -1,66 +1,58 @@
 package vista;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class PanelEscena1 extends JPanel implements Runnable{
+public class PanelEscena1 extends JPanel implements KeyListener{
 
 	private static final long serialVersionUID = 1L;
-	private Image imagen, jugador;
-
+	private Image imgSuelo, jugador, imgPared, imgExplosion, imgBomba;
+	private final int TAMANO_CELDA = 70;
+	private int[][] mapa; // Mapa de valores 0 y 1
 	private int x = 1;//editado
 	private int y = 1;//editado
 	private final int ANCHO = 50;
 	private final int ALTO = 50;
-	private int velocidad = 5;
+	private int velocidad = 1;
 
-
-	private Thread hilo;
-
-	//Variables para el movimiento
-	private boolean arriba, abajo, izquierda, derecha;	
-
+	// VARIABLES BOMBA
+	private boolean bombaActiva = false;
+	private boolean explosion = false;
+	private int bombaX;
+	private int bombaY;
+	private boolean muerto = false;
+	
 	public PanelEscena1() {
 
-		// Imagen del fondo del juego
-		imagen = new ImageIcon(getClass().getResource("/utils/escenario.png")).getImage();
-		jugador = new ImageIcon(getClass().getResource("/utils/barcelona.png")).getImage();
+		mapa = new int[11][17];
+		for (int i = 0; i < mapa.length; i++) {
+			for (int j = 0; j < mapa[i].length; j++) {
+				mapa[i][j] = (int) Math.round(Math.random());
+			}
+		}
+		mapa[y][x] = 0;
+
+		//Imagen del fondo del juego
+
+		jugador = new ImageIcon(getClass().getResource("/utils/vecna.png")).getImage();
+		imgPared = new ImageIcon(getClass().getResource("/utils/suelopiedrasombra.png")).getImage();
+		imgSuelo = new ImageIcon(getClass().getResource("/utils/arenasuelo.png")).getImage();
+		imgExplosion = new ImageIcon(getClass().getResource("/utils/explosion.png")).getImage();
+		imgBomba = new ImageIcon(getClass().getResource("/utils/bomba.png")).getImage();
+
 
 		setFocusable(true); //Muy importante ya que si no lo ponemos, no reacciona a las pulsaciones del teclado
-
-		// Listener de teclado
-		addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				switch (e.getKeyCode()) {
-				case KeyEvent.VK_RIGHT -> derecha = true;
-				case KeyEvent.VK_LEFT  -> izquierda = true;
-				case KeyEvent.VK_UP    -> arriba = true;
-				case KeyEvent.VK_DOWN  -> abajo = true;
-				}
-			}
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-				switch (e.getKeyCode()) {
-				case KeyEvent.VK_RIGHT -> derecha = false;
-				case KeyEvent.VK_LEFT  -> izquierda = false;
-				case KeyEvent.VK_UP    -> arriba = false;
-				case KeyEvent.VK_DOWN  -> abajo = false;
-				}
-			}
-		});
-
-		// Iniciar hilo del juego
-		hilo = new Thread(this);
-		hilo.start();
+		addKeyListener(this);
 	}
+
 
 	@Override
 	public void addNotify() {
@@ -73,35 +65,140 @@ public class PanelEscena1 extends JPanel implements Runnable{
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
-		g.drawImage(jugador, x, y, 100, 100, this); //posición x, y, tamaño del personaje
 
+		for (int i = 0; i < mapa.length; i++) {
+			for (int j = 0; j < mapa[i].length; j++) {
+				if (mapa[i][j] == 1) {
+					g.fillRect(j * TAMANO_CELDA, i * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA);
+					g.drawImage(imgPared, j * TAMANO_CELDA, i * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA, this);
+				} else {
+					g.drawImage(imgSuelo, j * TAMANO_CELDA, i * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA, this);
+
+				}
+			}
+		} 
+
+		g.drawImage(jugador, x * TAMANO_CELDA, y * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA, this);
+
+		// DIBUJAR BOMBA
+		if (bombaActiva && !explosion) {
+			g.drawImage(imgBomba, bombaX * TAMANO_CELDA, bombaY * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA, this);
+			
+		}
+
+		// DIBUJAR EXPLOSION
+		// DIBUJAR EXPLOSION TIPO BOMBERMAN
+		if (explosion) {
+
+			// centro
+			g.drawImage(imgExplosion, bombaX * TAMANO_CELDA, bombaY * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA, this);
+
+			// derecha
+			g.drawImage(imgExplosion, (bombaX + 1) * TAMANO_CELDA, bombaY * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA, this);
+
+			// izquierda
+			g.drawImage(imgExplosion, (bombaX - 1) * TAMANO_CELDA, bombaY * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA, this);
+
+			// abajo
+			g.drawImage(imgExplosion, bombaX * TAMANO_CELDA, (bombaY + 1) * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA, this);
+
+			// arriba
+			g.drawImage(imgExplosion, bombaX * TAMANO_CELDA, (bombaY - 1) * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA, this);
+		}
+		
 	}
 
-	@Override
-	public void run() {
-		while (true) {
-			actualizarPosicion();
-			repaint();
 
-			try {
-				Thread.sleep(16); 
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int newX = x;
+		int newY = y;
+
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			newY--;
+			break;
+		case KeyEvent.VK_DOWN:
+			newY++;
+			break;
+		case KeyEvent.VK_LEFT:
+			newX--;
+			break;
+		case KeyEvent.VK_RIGHT:
+			newX++;
+			break;
+		case KeyEvent.VK_SPACE:
+
+			if (!bombaActiva) {
+				bombaActiva = true;
+				bombaX = x;
+				bombaY = y;
+
+				repaint();
+
+				new Thread(() -> {
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException ex) {}
+
+					explosion = true;
+
+					// ROMPER BLOQUES
+					romperBloques();
+
+					repaint();
+
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException ex) {}
+
+					explosion = false;
+					bombaActiva = false;
+					repaint();
+
+				}).start();
+			}
+
+			break;
+		}
+
+		if (newX >= 0 && newX < mapa[0].length && newY >= 0 && newY < mapa.length && mapa[newY][newX] == 0) {
+			x = newX;
+			y = newY;
+			repaint();
+		}
+	}
+
+	// METODO PARA ROMPER BLOQUES
+	private void romperBloques() {
+
+		int[][] direcciones = {
+				{0,0},
+				{1,0},
+				{-1,0},
+				{0,1},
+				{0,-1}
+		};
+
+		for (int[] d : direcciones) {
+
+			int nx = bombaX + d[0];
+			int ny = bombaY + d[1];
+
+			if (nx >= 0 && nx < mapa[0].length && ny >= 0 && ny < mapa.length) {
+
+				if (mapa[ny][nx] == 1) {
+					mapa[ny][nx] = 0;
+				}
+
 			}
 		}
 	}
 
-	private void actualizarPosicion() {
-		if (derecha && x + ANCHO < getWidth()) x += velocidad;
-		if (izquierda && x > 0) x -= velocidad;
-		if (arriba && y > 0) y -= velocidad;
-		if (abajo && y + ALTO < getHeight()) y += velocidad;
-	}
+	@Override
+	public void keyReleased(KeyEvent e) {}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
 
 }
-
-
-
-
-
